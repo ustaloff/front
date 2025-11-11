4d6f7139233c065755cd0671cab5781748c69328
Kirills Ustalovs <kirils.u@rdvit.com>
2025-11-11 16:44:40 +0200

    Custom slider

--- src/components/PrimeVueDemo.vue
+++ src/components/PrimeVueDemo.vue
@@ -1,7 +1,9 @@
 <template>
     <PromoSlider />
+    <MainSlider />
 </template>
 
 <script setup>
 import PromoSlider from '@/components/ui/PromoSlider.vue';
+import MainSlider from '@/components/ui/MainSlider.vue';
 </script>
